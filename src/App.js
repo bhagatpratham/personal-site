84a76cce6b276537440572345d2af0cc82bbc7c4
Pratham <bhagatpratham101@gmail.com>
2022-03-05 16:13:36 +0530

    setting up my site :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import Links from "./components/Links";
+import Setup from "./components/Setup";
+import Summaries from "./components/Summaries";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav-links">
+        <ul>
+          <li className="name">Pratham Bhagat</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/links">Links</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/summaries">Summaries</Link>
+          </li>
+          <li>
+            <Link to="/setup">Setup</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/links" element={<Links />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/summaries" element={<Summaries />} />
+        <Route path="/setup" element={<Setup />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Summaries.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Summaries = () => {
+  return <div>Summaries</div>;
+};
+
+export default Summaries;
